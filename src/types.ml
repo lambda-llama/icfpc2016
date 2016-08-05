2597ba9a77489d2e6d036645cc374b547757aabe
Aleksey Kladov <aleksey.kladov@gmail.com>
2016-08-05 12:03:49 +0300

    Some more types

--- src/types.ml
+++ src/types.ml
@@ -2,3 +2,24 @@ type expr =
   | Num of int
   | Add of expr * expr
 [@@deriving show]
+
+
+type problem_spec = polygon list
+
+type polygon = vertext list
+
+type vertex = (frac * frac)
+
+type frac = (int * int)
+(* 1 *)
+(* 4 *)
+(* 0,0 *)
+(* 1,0 *)
+(* 1/2,1/2 *)
+(* 0,1/2 *)
+(* 5 *)
+(* 0,0 1,0 *)
+(* 1,0 1/2,1/2 *)
+(* 1/2,1/2 0,1/2 *)
+(* 0,1/2 0,0 *)
+(* 0,0 1/2,1/2 *)
