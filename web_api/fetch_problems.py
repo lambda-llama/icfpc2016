a6db3ce777adeaf1ffc4c56703c22a4f6e0920fd
Egor Panfilov <multicolor.mood@gmail.com>
2016-08-06 22:19:56 +0300

    Added meta-info for problems

--- web_api/fetch_problems.py
+++ web_api/fetch_problems.py
@@ -5,7 +5,7 @@ from glob import glob
 from api import request_snapshots, request_blob
 
 
-MAX_TO_DOWNLOAD = 100
+MAX_TO_DOWNLOAD = 200
 
 
 if __name__ == '__main__':
@@ -23,13 +23,14 @@ if __name__ == '__main__':
     tmp_blob = request_blob(last_snap_hash).json()
 
     problems_curr_d = {e['problem_id']: (e['publish_time'],
-                                       e['problem_spec_hash'])
+                                         e['problem_spec_hash'])
                      for e in tmp_blob['problems']}
     
     problems_old = set([int(os.path.basename(e)
                             .replace('problem', '')
                             .replace('.txt', ''))
                         for e in glob('../problems/*.txt')])
+    # TODO: enhance matching via using `spec_hash` and `publish_time`
     problems_todo = set(problems_curr_d.keys()).difference(problems_old)
     print('New problems found: {}'.format(problems_todo))
 
@@ -40,6 +41,12 @@ if __name__ == '__main__':
         tmp_problem = request_blob(problems_curr_d[problem_todo][1]).text
         with open(tmp_filename, 'w') as f:
             f.writelines(tmp_problem)
+
+        tmp_filename = '../problems/problem{}.meta'.format(problem_todo)
+        meta = problems_curr_d[problem_todo]
+        with open(tmp_filename, 'w') as f:
+            f.writelines([str(meta[0]), '\n', meta[1], '\n'])
+        
         print('Problem {} has been downloaded.'.format(problem_todo))
 
         if idx == MAX_TO_DOWNLOAD:
