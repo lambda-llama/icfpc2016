3b7b3337749f1ec93b16b65d577359b25af0c33a
Egor Panfilov <multicolor.mood@gmail.com>
2016-08-06 20:46:43 +0300

    Added script to batch download new problems

---
+++ web_api/fetch_problems.py
@@ -0,0 +1,47 @@
+import os
+import time
+from glob import glob
+
+from api import request_snapshots, request_blob
+
+
+MAX_TO_DOWNLOAD = 100
+
+
+if __name__ == '__main__':
+    tmp_snaps = request_snapshots().json()
+
+    last_snap = tmp_snaps['snapshots'][-1]
+    last_snap_hash = last_snap['snapshot_hash']
+    last_snap_time = last_snap['snapshot_time']
+    print('Latest snapshot:\n hash:{}\n time:{}'.format(
+        last_snap_hash, last_snap_time))
+
+    # To avoid "429: Too Many Requests"
+    time.sleep(1)
+    
+    tmp_blob = request_blob(last_snap_hash).json()
+
+    problems_curr_d = {e['problem_id']: (e['publish_time'],
+                                       e['problem_spec_hash'])
+                     for e in tmp_blob['problems']}
+    
+    problems_old = set([int(os.path.basename(e)
+                            .replace('problem', '')
+                            .replace('.txt', ''))
+                        for e in glob('../problems/*.txt')])
+    problems_todo = set(problems_curr_d.keys()).difference(problems_old)
+    print('New problems found: {}'.format(problems_todo))
+
+    for idx, problem_todo in enumerate(problems_todo):
+        time.sleep(1)
+        
+        tmp_filename = '../problems/problem{}.txt'.format(problem_todo)
+        tmp_problem = request_blob(problems_curr_d[problem_todo][1]).text
+        with open(tmp_filename, 'w') as f:
+            f.writelines(tmp_problem)
+        print('Problem {} has been downloaded.'.format(problem_todo))
+
+        if idx == MAX_TO_DOWNLOAD:
+            print('Artificial download limit exceeded.')
+            quit()
--- web_api/submit_all.py
+++ web_api/submit_all.py
@@ -1,10 +1,11 @@
-from submitter import submit
 import time
+from api import submit_problem
 
 
 if __name__ == '__main__':
     prob_fst = 1
     prob_last = 101
+
     for i in range(prob_fst, prob_last):
-        print(submit(str(i), "solution1.txt").text)
+        print(submit_problem(i, "solution1.txt").text)
         time.sleep(2)
