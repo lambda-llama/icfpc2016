ac58d2d0c52d05208999756816d623557e97865f
Sergei Lebedev <superbobry@gmail.com>
2016-08-07 16:19:21 +0300

    Added more exact solutions

--- web_api/submit_all.py
+++ web_api/submit_all.py
@@ -1,11 +1,31 @@
+import os.path
+import subprocess
 import time
-from api import submit_problem
+
+from api import submit_solution
 
 
 if __name__ == '__main__':
-    prob_fst = 1
-    prob_last = 101
+    prob_fst = 103
+    prob_last = 5000
 
     for i in range(prob_fst, prob_last):
-        print(submit_problem(i, "solution1.txt").text)
-        time.sleep(2)
+        print("problem {:04d}:".format(i), end=" ")
+        if not os.path.exists("problems/problem{}.txt".format(i)):
+            print("SKIP")
+            continue
+
+        solution_path = "solutions/solution{}.txt".format(i)
+        if not os.path.exists(solution_path):
+            try:
+                subprocess.check_call(["./solver.native", str(i)],
+                                      stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE,
+                                      timeout=5)
+            except subprocess.TimeoutExpired:
+                print("FAILED")
+                continue
+
+        print("OK " + solution_path)
+        print(submit_solution(i, solution_path).text)
+        time.sleep(5)
--- web_api/submitter.py
+++ web_api/submitter.py
@@ -2,24 +2,9 @@
 import sys
 import requests
 
-
-def submit(problem_id, file):
-    headers = {
-        "X-API-Key": "179-c628d31b740634928666a1515aeb12a2"
-    }
-    api_url = "http://2016sv.icfpcontest.org/api/solution/submit"
-    with open(file) as f:
-        files = {
-            "problem_id": problem_id,
-            "solution_spec": f
-        }
-        resp = requests.post(
-            api_url, headers=headers, files=files
-        )
-    return resp
-
+from api import submit_solution
 
 if __name__ == '__main__':
     _, problem_id = sys.argv
     path = "solutions/solution{}.txt".format(problem_id)
-    print(submit(problem_id, path).text)
+    print(submit_solution(problem_id, path).text)
